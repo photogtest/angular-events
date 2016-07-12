54172ae60a5d88d32b8c25d6adc875bd47d24d5d
Nicolas Taglienti <nicolas@serfe.com>
2016-07-12 15:06:34 -0300

    angular aplication first approach

---
+++ src/app/index.module.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularEvents', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize',
+'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr', 'ui.calendar']);
+
+})();
---
+++ src/app/main/main.controller.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularEvents')
+    .controller('MainController', MainController);
+
+  /** @ngInject */
+  function MainController() {
+       
+        var self = this;
+
+        /////// VARS ////////
+
+        /////// METHODS ////////
+
+        activate();
+        /////// DEFINITIONS ////////
+
+
+        function activate() {
+        
+        }
+        
+        
+
+  }
+})();
