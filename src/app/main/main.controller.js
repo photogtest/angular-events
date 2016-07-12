352729289144c7e38b72045e0d6b7b7798c9ead8
Nicolas Taglienti <nicolas@serfe.com>
2016-07-12 18:09:57 -0300

    refresh detailed data

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -13,20 +13,152 @@
         /////// VARS ////////
         self.events         = [];
         self.detailedData   = [];
-        self.currentDay     = '2016/06/12';
+        self.currentDay     = '';
         self.view = 'month';
         self.today = moment().startOf('month').toDate();
         
         /////// METHODS ////////
         self.dateClicked = dateClicked;
-        self.getDateData = getDateData;
+        self.setDetailedData = setDetailedData;
         self.resetData   = resetData;
         activate();
         /////// DEFINITIONS ////////
 
 
         function activate() {
-            
+            self.events = [
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').toDate()
+                },
+                {
+                    title: 'Non-draggable event',
+                    type: 'info',
+                    startsAt: moment().startOf('month').add(1, 'day').toDate()
+                }
+            ];
+            self.setDetailedData(moment());
         }
         
         /**
@@ -36,7 +168,7 @@
          * @returns {undefined}
          */
         function dateClicked(date){
-            console.log('Click date: ' + date);
+            self.setDetailedData(date);
         }
         /**
          * get the events data
@@ -44,9 +176,19 @@
          * @param {type} date
          * @returns {undefined}
          */
-        function getDateData(date){
-            
+        function setDetailedData(date){
+            self.detailedData   = [];
+            var formatedDate = moment(date);
+            self.currentDay = formatedDate.format("MMM Do YY");
+            angular.forEach(self.events, function(event) {
+                var diff = date - event.startsAt;
+                if (diff == 0){
+                    self.detailedData.push(event)
+                }
+            });
         }   
+        
+        
         /**
          * Delete all event for this user.
          * 
